1cb01c7213aa4c8665b93800465c82b24d5750ab
hanadaUG <hanadaUG@gmail.com>
2021-01-30 01:29:44 +0900

    コマンドライン引数を追加

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
-package file_viewer
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	// コマンドライン引数
+	{
+		root := flag.String("root", "path/to/hoge", "Specify root directory path to open.")
+		port := flag.Int("port", 8000, "Specify port to use.")
+		flag.Parse()
+		fmt.Printf("root: %s\n", *root)
+		fmt.Printf("port: %d\n", *port)
+	}
+}
