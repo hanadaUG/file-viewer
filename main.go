74bd6813f47c5db97ede8f113be88c2687656237
hanadaUG <hanadaUG@gmail.com>
2021-01-30 02:50:40 +0900

    ファイル情報を表現するEntity 構造体を作成

--- main.go
+++ main.go
@@ -4,10 +4,31 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"path/filepath"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
+
+type FileType string
+
+const (
+	Dir     = FileType("directory")
+	Jpeg    = FileType("jpeg")
+	Png     = FileType("png")
+	Txt     = FileType("txt")
+	Json    = FileType("json")
+	UnKnown = FileType("unknown")
+)
+
+type Entity struct {
+	Path     string
+	FileType FileType
+}
+
+func newEntity(path string) *Entity {
+	return &Entity{Path: path, FileType: getFileType(path)}
+}
 
 func main() {
 	// コマンドライン引数
@@ -35,4 +56,27 @@ func main() {
 // Handler
 func handler(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
+}
+
+// 拡張子からファイル種類を判別
+func getFileType(path string) FileType {
+	ext := filepath.Ext(path) // "path/to/hoge.c" => ".c"
+	fmt.Printf("ext: %s\n", ext)
+
+	switch ext {
+	case "":
+		return Dir
+	case ".jpeg":
+		return Jpeg
+	case ".jpg":
+		return Jpeg
+	case ".png":
+		return Png
+	case ".txt":
+		return Txt
+	case ".json":
+		return Json
+	default:
+		return UnKnown
+	}
 }
